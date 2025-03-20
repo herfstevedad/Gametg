ffd406dbfec0d38d74f7c2e70acfb9be60d66022
Неминущий Максим Иванович <neminuschiy@DRAFT.LOCAL>
2025-03-20 13:16:46 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
+import { init, miniApp } from '@telegram-apps/sdk'
+import Registration from './components/registration/registration';
 
 import './App.css'
 
 function App() {
-  //Переменные
-
-  //Переменные
 
   //Запуск TelegramSDK
- 
+  const initializeTelegramSDK = async () => {
+    try {
+      await init();
+
+      if (miniApp.ready.isAvailable()) {
+        await miniApp.ready()
+      }
+    } catch (error) {
+      console.error('Ошибка инициализации:', error);
+    }
+  }
+
+  initializeTelegramSDK();
+  //Запуск TelegramSDK
+
 
   return (
     <div>
-      1
+      <Registration />
     </div>
-  );
+  )
 };
 
 export default App
